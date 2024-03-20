af4caf12759c8da41fbdd1734c875095a55b0ef6
sqx101 <145257477+sqx101@users.noreply.github.com>
2024-03-20 15:13:05 -0500

    moved config .ts file

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-// src/config.ts
-export const CONFIG = {
-    SITE_URL: 'https://zora.co/collect/base:0x7d958776cb5355bfe7737b55ae1cf8a5af2ab8be/1',
-    CHAIN: 'eip155:8453', // Chain ID for Base Mainnet
-    CONTRACT_ADDRESS: '0x7d958776CB5355bFE7737B55AE1cF8a5AF2AB8bE', // Your Zora collection's contract address
-    TOKEN_ID: 1, // Assuming first collection token ID is 1
-    // ... any other configuration constants
-};
